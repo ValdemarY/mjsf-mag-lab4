9175da8d49787d8f7e0b1aea9be5ff0f8be55142
ValdemarY <32068893+ValdemarY@users.noreply.github.com>
2020-10-30 14:37:40 +0200

    Alert

--- ex3-modal-component/src/views/home/Home.vue
+++ ex3-modal-component/src/views/home/Home.vue
@@ -17,23 +17,47 @@
     <b-modal v-model="showModal" close-btn size="xs" title="My title" @click:outside="showModal = false">
       Modal with model
     </b-modal>
-
     <button type="button" class="btn btn-primary" @click="showModal = !showModal">
       Launch demo modal
     </button>
+    <hr>
+    
+    <b-alert :show="dismissCountDown" variant="warning" dismissible @start="showDismissableAlert" @close="closeDismissableAlert" @dismiss-countdown="countDownChanged">
+      This alert will dismiss after {{ dismissCountDown }} seconds...
+      <template #activator="{on}">
+        <button type="button" class="btn btn-primary" @click="on">
+          Launch alert
+        </button>
+      </template>
+    </b-alert>
+
   </div>
 </template>
 
 <script>
   import BModal from "../../components/bootstrap/BModal";
+  import BAlert from "../../components/bootstrap/BAlert";
   import PageHeader from "./PageHeader";
 
   export default {
     name: "Home",
-    components: {PageHeader, BModal},
+    components: {PageHeader, BModal, BAlert},
     data() {
       return {
-        showModal: false
+        showModal: false,
+        dismissSecs: 5,
+        dismissCountDown: 0,
+      }
+    },
+    methods:{
+      showDismissableAlert(){
+        this.dismissCountDown = this.dismissSecs;
+      },
+      closeDismissableAlert(){
+        this.dismissCountDown = 0;
+      },
+      countDownChanged(newDismissCountdown){
+        this.dismissCountDown = newDismissCountdown;
       }
     }
   }
